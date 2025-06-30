b98183e1cb05ef5d36625109610942abfe04845d
eduardo239 <eucrieiumaconta@gmail.com>
2025-06-29 22:18:23 -0300

    adicionado etapa de testes

---
+++ src/components/Button.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Button component", () => {
+  it("renders correctly", () => {
+    render(<Button>Click me</Button>);
+    expect(screen.getByText("Click me")).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/", // ou seu path base se estiver fazendo deploy em subdiretório
   build: {
     outDir: "dist",
     emptyOutDir: true,
+  },
+  // @ts-ignore - Ignora o erro de tipo temporariamente
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
 });
