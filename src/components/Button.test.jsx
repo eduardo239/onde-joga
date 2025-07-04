07de62ac6c6a55e6ad5352358d72308534c84a89
eduardo239 <eucrieiumaconta@gmail.com>
2025-07-04 09:50:20 -0300

    adicionado teste padrão

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -1,9 +1,10 @@
-// import { render, screen } from "@testing-library/react";
-// import Button from "./Button";
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
 
-// describe("Button component", () => {
-//   it("renders correctly", () => {
-//     render(<Button>Click me</Button>);
-//     expect(screen.getByText("Click me")).toBeInTheDocument();
-//   });
-// });
+describe("Button component", () => {
+  it("renders correctly", () => {
+    render(<Button />);
+    // render(<Button>Click me</Button>);
+    // expect(screen.getByText("Click me")).toBeInTheDocument();
+  });
+});
